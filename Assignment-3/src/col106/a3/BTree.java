b037fc7c0f81e75701f07de7210e377061f322db
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-10-24 13:28:54 +0530

    Create the basic private classes and basic functions

--- Assignment-3/src/col106/a3/BTree.java
+++ Assignment-3/src/col106/a3/BTree.java
@@ -1,26 +1,52 @@
 package col106.a3;
 
+
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class BTree<Key extends Comparable<Key>,Value> implements DuplicateBTree<Key,Value> {
 
+    private int B;
+    private int n = 0; /* Number of Key-value pairs stored */
+    private int height = 0;
+    private Node root;
+
+    private class Entry {
+        private Key key;
+        private Value val;
+
+        public Entry(Key key, Value val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    private class Node {
+        private int m = 0;
+        private List<Entry> entries  = new ArrayList<Entry>();
+        private List<Node>  children = new ArrayList<Node> ();
+    }
+
     public BTree(int b) throws bNotEvenException {  /* Initializes an empty b-tree. Assume b is even. */
-        throw new RuntimeException("Not Implemented");
+        if ((b & 1) != 0) throw new bNotEvenException();
+        this.B = b;
+        this.root = new Node();
     }
 
     @Override
     public boolean isEmpty() {
-        throw new RuntimeException("Not Implemented");
+        return this.n == 0;
     }
 
     @Override
     public int size() {
-        throw new RuntimeException("Not Implemented");
+        return this.n;
     }
 
     @Override
     public int height() {
-        throw new RuntimeException("Not Implemented");
+        return this.height;
     }
 
     @Override
