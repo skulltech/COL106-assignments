a389bd83429fc0320d818c1b59715ece9b568143
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-10-28 17:41:04 +0530

    Modify the constructor. Make the table store buckets.

--- Assignment-4/src/HashTable.java
+++ Assignment-4/src/HashTable.java
@@ -1,10 +1,34 @@
 public class HashTable {
-    public static int hash(String s, int size) {
+    private bucket[] table;
+    private int size;
+
+    private class bucket {
+        String key;
+        Boolean val;
+        bucket next;
+
+        public bucket(String key, Boolean val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    private int hash(String s) {
         int hash = 0;
         for (int i=0; i<s.length(); i++) {
-            hash = ((int) s.charAt(i) + 256*hash) % size;
+            hash = ((int) s.charAt(i) + 256*hash) % this.size;
         }
         return hash;
+    }
+
+    public HashTable(int size) {
+        float loadFactor = 0.75f;
+        this.size = (int) ((float)size/loadFactor);
+        this.table = new bucket[this.size];
+    }
+
+    public void insert(String s) {
+
     }
 
     public static void main(String[] args) {
