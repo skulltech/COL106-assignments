a85d5bdb4e4524a745b8358946fe468928e511fa
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-10-29 14:16:37 +0530

    Create basic functions for Anagram

--- Assignment-4/src/Anagram.java
+++ Assignment-4/src/Anagram.java
@@ -1,2 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Anagram {
+    private HashTable table;
+
+    public Anagram(String filePath) throws FileNotFoundException{
+        File file = new File(filePath);
+        Scanner s = new Scanner(file);
+
+        int size = s.nextInt();
+        s.nextLine();
+        this.table = new HashTable(size);
+
+        for (int i=0; i<size; i++) { table.insert(s.nextLine()); }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
+        Anagram anagram = new Anagram("C:\\Users\\Sumit\\Documents\\Coding\\COL106-assignments\\Assignment-4\\src\\vocabulary.txt");
+    }
 }
