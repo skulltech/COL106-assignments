6988fab92cf35af9791d6527d187ab68262150a4
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-12 14:50:29 +0530

    Create SlidingPuzzleGraph.java

---
+++ Assignment-5/src/SlidingPuzzleGraph.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class SlidingPuzzleGraph {
+    private class Vertex {
+        private Board board;
+        private Boolean known;
+        private int distance;
+    }
+
+    public SlidingPuzzleGraph(int[][] initial) {
+    }
+
+    public SlidingPuzzleGraph(String initial) {
+    }
+}
